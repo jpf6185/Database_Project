eee55c52eebc1ab08202dd7e5f169733b06193a8
Jacob Feiner <jac.feiner@gmail.com>
2018-03-31 16:51:53 -0400

    added CapstoneInfo class and started working on Capstone version class

--- src/CapstoneInfo.java
+++ src/CapstoneInfo.java
@@ -1,15 +1,53 @@
-import java.util.*;
-
-
+import java.util.ArrayList;
 
 public class CapstoneInfo{
 
     private String CapstoneID;
-    private String "Lap"
-
+    private String LatestDate;
+    private String PlagiarismScore;
+    private String Grade;
+    private String DefenseDate;
     public CapstoneInfo(String _CapstoneID){
         CapstoneID=_CapstoneID;
     }
 
+    public String getCapstoneID() {
+        return CapstoneID;
+    }
 
+    public void setCapstoneID(String capstoneID) {
+        CapstoneID = capstoneID;
+    }
+
+    public String getLatestDate() {
+        return LatestDate;
+    }
+
+    public void setLatestDate(String latestDate) {
+        LatestDate = latestDate;
+    }
+
+    public String getPlagiarismScore() {
+        return PlagiarismScore;
+    }
+
+    public void setPlagiarismScore(String plagiarismScore) {
+        PlagiarismScore = plagiarismScore;
+    }
+
+    public String getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(String grade) {
+        Grade = grade;
+    }
+
+    public String getDefenseDate() {
+        return DefenseDate;
+    }
+
+    public void setDefenseDate(String defenseDate) {
+        DefenseDate = defenseDate;
+    }
 }
---
+++ src/CapstoneVersion.java
@@ -0,0 +1,15 @@
+
+
+public class CapstoneVersion{
+    private String Date;
+    private String CapstoneID;
+    private String Status;
+    private String Title;
+    private String Description;
+
+        public CapstoneVersion(String _CapstoneID,String _Date){
+            Date=_Date;
+            CapstoneID=_CapstoneID;
+        }
+
+}
