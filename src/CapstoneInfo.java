5a193593fe0f75438e6de4b98952aca5f185b319
Jacob Feiner <jac.feiner@gmail.com>
2018-03-31 15:50:05 -0400

    added first buisness layer class

---
+++ src/CapstoneInfo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+
+public class CapstoneInfo{
+
+    private String CapstoneID;
+    private String "Lap"
+
+    public CapstoneInfo(String _CapstoneID){
+        CapstoneID=_CapstoneID;
+    }
+
+
+}
