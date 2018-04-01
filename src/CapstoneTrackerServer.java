c870bfe9154eb2bfe39ec18812f3a7630dc1b001
Jacob Feiner <jac.feiner@gmail.com>
2018-04-01 14:40:52 -0400

    added capstone tracker database

---
+++ src/CapstoneTrackerServer.java
@@ -0,0 +1,17 @@
+
+
+
+public class CapstoneTrackerServer{
+
+
+    public CapstoneInfo GetCapstoneInfo(CapstoneInfo){
+        try{
+            String sqlStatement="Select * From Capstone_Info JOIN "
+        }
+        catch(DLException dle){
+            System.out.println("An error occured attempting to get the info on a capstone")
+        }
+
+    }
+
+}
