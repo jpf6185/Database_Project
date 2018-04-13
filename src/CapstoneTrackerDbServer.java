ce1560f1f188d3209fbb6d64faf14dcd56a11084
Jacob Feiner <jac.feiner@gmail.com>
2018-04-13 16:27:43 -0400

    added capstone tracker serverclass

---
+++ src/CapstoneTrackerDbServer.java
@@ -0,0 +1,22 @@
+
+/* this class determinne what function needs to be called and uses capstonetrackerDBInterface to do so
+* This is needed because ultimately the client and the server will be comuncating over the network and thus the client
+*  cannot directly call anything on the server
+*/
+public class CapstoneTrackerDbServer {
+
+    private CapstoneTrackerDBInterface dbInterface;
+
+
+    // constructor that creates a interface for this instance
+    public CapstoneTrackerDbServer(){
+        dbInterface=new CapstoneTrackerDBInterface();
+    }
+
+    // method that
+
+
+    public
+
+
+}
