254fe7f6cacb86c8200b903c5cb6967cfd9f259c
Jacob Feiner <jac.feiner@gmail.com>
2018-04-22 13:30:50 -0400

    added a new class to store infomation about statuses

---
+++ src/StatusInfo.java
@@ -0,0 +1,26 @@
+public class StatusInfo {
+    private String StatusName;
+    private String StatusNumber;
+
+    public StatusInfo(String statusName, String statusNumber) {
+        StatusName = statusName;
+        StatusNumber = statusNumber;
+    }
+    public StatusInfo(){}
+
+    public String getStatusName() {
+        return StatusName;
+    }
+
+    public void setStatusName(String statusName) {
+        StatusName = statusName;
+    }
+
+    public String getStatusNumber() {
+        return StatusNumber;
+    }
+
+    public void setStatusNumber(String statusNumber) {
+        StatusNumber = statusNumber;
+    }
+}
